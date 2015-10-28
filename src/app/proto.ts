743a9ee0c12fa40ca0a3a2abef91686dca4c6ce7
Franz Junge <bitbucket@junge.f9.co.uk>
2015-10-28 10:51:58 +0000

    Added pipes illustration

--- src/app/proto.ts
+++ src/app/proto.ts
@@ -3,6 +3,7 @@ import { ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
 import { HomeComponent } from 'app/snippets/home';
 import { InputsAndOutputs } from 'app/snippets/01-input-and-output/inputs-and-outputs';
 import { FormsSnippet } from 'app/snippets/02-forms/forms-snippet';
+import { PipesSnippet } from 'app/snippets/03-pipes/pipes-snippet';
 
 @Component({
     selector: 'proto'
@@ -21,6 +22,7 @@ import { FormsSnippet } from 'app/snippets/02-forms/forms-snippet';
   { path: '/Home', component: HomeComponent, as: 'Home' },
 	{ path: '/01-input-and-output', component: InputsAndOutputs, as: "InputAndOutputSnippet"},
 	{ path: '/02-forms', component: FormsSnippet, as: "FormsSnippet"}
+	{ path: '/03-pipes', component: PipesSnippet, as: "PipesSnippet"}
 ])
 
 export class MyAppComponent {
--- src/app/snippets/home.ts
+++ src/app/snippets/home.ts
@@ -9,6 +9,7 @@ import { Component, View, NgIf, Injector } from 'angular2/angular2';
 		<ol>
 			<li><a href="#/01-input-and-output">Inputs &amp; Outputs</a></li>
 			<li><a href="#/02-forms">Forms</a></li>			
+			<li><a href="#/03-pipes">Pipes</a></li>			
 		</ol>
 	`,
 	directives: []
