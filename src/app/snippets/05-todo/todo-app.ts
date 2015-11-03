01f67c334341c9ad16bf638fdd24ad3b70a7f034
Franz Junge <bitbucket@junge.f9.co.uk>
2015-11-03 12:38:38 +0000

    Added more documentation for the ToDo snippet

--- src/app/snippets/05-todo/todo-app.ts
+++ src/app/snippets/05-todo/todo-app.ts
@@ -9,7 +9,37 @@ import { TaskItem, CommandTypes, Command } from "./models";
 @View({
 	template: `
 		<h2>ToDo Application</h2>
-
+		
+		<p>
+			Brings together key concepts for building Angular2 apps, including:			
+		</p>
+		<ul>
+			<li>Breaking down an app into components</li>
+			<li>Component inheritance (and possible approach to component style inhertiance)</li>
+			<li>Using models</li>
+			<li>Component communication with inputs &amp; outputs</li>
+			<li>Using a [root] component orchestrator (for communication)</li>
+			<li>View/Edit mode handling</li>
+		</ul>
+		
+		<h3>Issues - Component Inheritance</h3>
+		<p>
+			When using <i>Component Inheritance</i> use the <strong>inputs: ["xyz"]</strong> form
+			for defining inputs &amp; outputs. 
+		</p>
+		<p>
+			When using the <strong>@Input ...</strong> approach you end up with two distinct variables
+			- the one in your component, and one in the base (aka super) version.  This isn't what we want
+			- using the <strong>inputs: ["sadf"]</strong> method the input is created via DI, and Angular
+			in essence creates it in the super class (which is what we <i>do</i> want).
+		</p>
+		
+		<h3>Resources</h3>
+		<p>n/a</p>
+		
+		<h3>Snippet</h3>
+		<hr/>
+		
 		<task-list [tasks]="masterList" (commander)="doCommand($event)"></task-list>
 	`,
 	directives: [TaskListComponent]
