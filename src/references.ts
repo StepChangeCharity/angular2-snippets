e43ab934a77360ab17c9a90ba555974f41dff90b
Franz Junge <bitbucket@junge.f9.co.uk>
2015-11-26 09:37:30 +0000

    Getting IE working (es6 shim required).

--- src/references.ts
+++ src/references.ts
@@ -1,6 +1,7 @@
 /// <reference path="../node_modules/angular2/angular2.d.ts" />
 /// <reference path="../node_modules/angular2/http.d.ts" />
 /// <reference path="../node_modules/angular2/router.d.ts" />
+/// <reference path="../node_modules/angular2/typings/es6-shim/es6-shim.d.ts" />
  
  
  
